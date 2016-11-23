958766a973ece60ff7edf690a3cdbd45c1de87e0
Felipe Machado <felipe.msz@gmail.com>
2016-11-23 19:11:21 +0100

    Add improvements to IdlePulleyHolder in parts.py

--- parts.py
+++ parts.py
@@ -44,6 +44,7 @@ logger = logging.getLogger(__name__)
 # ----------- class IdlePulleyHolder ---------------------------
 # Creates a holder for a IdlePulley. Usually made of bolts, washers and bearings
 # It may include a space for a endstop
+# It is centered at the idle pulley, but at the back, and at the profile height
 #
 #          hole for endstop
 #         /   []: hole for the nut
@@ -56,9 +57,10 @@ logger = logging.getLogger(__name__)
 #   __________________:________
 #
 #        O: holes for bolts to attach to the profile
-#
-#
-#        __________  ...
+#                 
+#               Z 
+#               :
+#        _______:__ ...
 #       /         /|   :
 #      /________ / |   :
 #      ||__|    |  |   + height
@@ -70,7 +72,7 @@ logger = logging.getLogger(__name__)
 #      :........:
 #          + width
 #
-#   attach_dir = '-y'  enstop_side= 1
+#   attach_dir = '-y'  enstop_side= 1     TOP VIEW
 #
 #
 #                Y
@@ -81,9 +83,6 @@ logger = logging.getLogger(__name__)
 #             | (:)       |
 #          ...|__:________|..... X
 #
-
-
-
 # ----- Arguments:
 # profile_size: size of the aluminum profile. 20mm, 30mm
 # pulleybolt_d: diameter of the bolt used to hold the pulley
@@ -93,6 +92,7 @@ logger = logging.getLogger(__name__)
 # mindepth: If there is a minimum depth. Sometimes needed for the endstop
 #            to reach its target
 # attach_dir: Normal vector to where the holder is attached:'x','-x','y','-y'
+#             NOW ONLY -y IS SUPPORTED. YOU CAN ROTATE IT
 # endstop_side: -1, 0, 1. Side where the enstop will be
 #                if attach_dir= 'x', this will be referred to the y axis 
 #                if 0, there will be no endstop
@@ -107,7 +107,7 @@ class IdlePulleyHolder (object):
 
     def __init__ (self, profile_size, pulleybolt_d, holdbolt_d, above_h,
                   mindepth = 0,
-                  attach_dir = 'x', endstop_side = 0,
+                  attach_dir = '-y', endstop_side = 0,
                   endstop_posh = 0,
                   name = "idlepulleyhold"):
 
@@ -123,6 +123,7 @@ class IdlePulleyHolder (object):
         extra_w = 4.
         # ----------------- Depth calculation
         pulleynut_d = kcomp.NUT_D934_D[int(pulleybolt_d)]
+        pulleynut_d_tol = pulleynut_d + 3*TOL
         pulleydepth = pulleynut_d + 2 * extra_w
         depth = max(pulleydepth, mindepth)
         if endstop_side != 0:
@@ -153,8 +154,8 @@ class IdlePulleyHolder (object):
         else:
             minwidth_endstop = (  endstop_l
                                 + 2*TOL
-                                + pulleybolt_d
-                                + 4*extra_w )
+                                + pulleynut_d_tol
+                                + 3*extra_w )
         width = max(minwidth_holdbolt, minwidth_endstop)
         self.width = width
 
@@ -175,6 +176,17 @@ class IdlePulleyHolder (object):
 #          ...|__:________|..... X
 #           p00          p01 
 #                 width
+#                :
+#      
+#                      Y
+#                      :
+#       p11    ________:__ p01
+#             | |__|   :  |
+#             |       (:) |        depth
+#          ...|________:__|..... X
+#           p10          p00 
+#                 width
+
 
         # Constants to dimensions
         # holding bolts that will be shank in the piece, the rest will be
@@ -194,26 +206,27 @@ class IdlePulleyHolder (object):
         pulleybolt_h = 2 * extra_w + pulleynut_hole_h
 
         if attach_dir == '-y':
-            if endstop_side == 1:
-                p0x = - pulleybolt_d/2. - extra_w
-                p1x =  p0x + width
-                p00 = FreeCAD.Vector ( p0x, 0, - base_h)
-                p01 = FreeCAD.Vector ( p1x, 0, - base_h)
-                p11 = FreeCAD.Vector ( p1x, depth, - base_h)
-                p10 = FreeCAD.Vector ( p0x, depth, - base_h)
+            if endstop_side == 0:
+                p0x = - width/2.
+                p1x = + width/2.
+                sg = 1 #sign
+            else:
+                sg = endstop_side # sign
+                p0x = sg * (- pulleynut_d_tol/2. - extra_w)
+                p1x =  p0x + sg * width
+            p00 = FreeCAD.Vector ( p0x, 0, - base_h)
+            p01 = FreeCAD.Vector ( p1x, 0, - base_h)
+            p11 = FreeCAD.Vector ( p1x, depth, - base_h)
+            p10 = FreeCAD.Vector ( p0x, depth, - base_h)
 
-                endstopbox_l = endstop_l + 2*TOL
-                endstopbox_w = endstop_ht + extra_endstop + TOL
-                endstop_posx = (p1x - extra_w - endstopbox_l )
-                endstop_posy = (depth - endstopbox_w )
 
             shp_wire_base = Part.makePolygon([p00,p01, p11, p10, p00])
             shp_face_base = Part.Face(shp_wire_base)
             shp_box = shp_face_base.extrude(FreeCAD.Vector(0,0,height))
 
 
-            hbolt_p0x = p0x + hbolt_endsep
-            #shank
+            hbolt_p0x = p0x + sg * hbolt_endsep
+            #shank of holding bolt
             pos_shank_hbolt0 = FreeCAD.Vector(hbolt_p0x,
                                               -1,
                                              -profile_size/2.)
@@ -234,12 +247,12 @@ class IdlePulleyHolder (object):
                 shp_hbolt0 = shp_shank_hbolt0 
             shp_hbolt1 = shp_hbolt0.copy()
             # It is in zero
-            shp_hbolt1.Placement.Base.x = hbolt_sep
+            shp_hbolt1.Placement.Base.x = sg * hbolt_sep
 
             # hole for the pulley bolt
             pulleybolt_pos = FreeCAD.Vector (0, depth - pulleydepth/2.,
                                              above_h - pulleybolt_h)
-            shp_pulleybolt = fcfun.shp_cyl (r = pulleybolt_d/2. + TOL,
+            shp_pulleybolt = fcfun.shp_cyl (r = pulleybolt_d/2. + 0.9*TOL/2,
                                             h = pulleybolt_h + 1,
                                             normal = VZ,
                                             pos = pulleybolt_pos)
@@ -249,17 +262,28 @@ class IdlePulleyHolder (object):
 
             # hole for the endstop
             if endstop_side != 0:
+                #endstopbox_l = endstop_l + 2*TOL
+                #endstopbox_w = endstop_ht + extra_endstop + TOL
+                endstopbox_l = endstop_l + 2*TOL + extra_w + 1
+                endstopbox_w = depth + 2
+                #endstop_posx = p1x + sg*(extra_w + endstopbox_l/2.)
+                endstop_posx = p1x - sg*(endstopbox_l/2. -1)
+                endstop_posy = (depth - endstopbox_w )
+                #endstop_posy = (depth - endstopbox_w )
+                endstop_posy = -1
+
                 endstop_pos = FreeCAD.Vector(endstop_posx, endstop_posy,
                                              endstop_posh)
                 shp_endstop = fcfun.shp_boxcen(x=endstopbox_l,
                                                y=endstopbox_w,
                                                z=above_h-endstop_posh +1,
+                                               cx=1,
                                                pos=endstop_pos)
                 holes_list.append(shp_endstop)
 
                 # hole for the bolts of the endstop
                 endstopbolt0_pos = FreeCAD.Vector(
-                                  endstop_posx + TOL + endstop_bolt2lend,
+                                  endstop_posx - endstop_boltsep/2.,
                                   depth - endstop_bolt2hend,
                                   endstop_posh + 1)
                 shp_endstopbolt0 = fcfun.shp_cyl (
@@ -279,7 +303,7 @@ class IdlePulleyHolder (object):
             pulleyhold_aux = doc.addObject("Part::Feature", name + '_aux')
             pulleyhold_aux.Shape = shp_pulleyhold
 
-            h_nuthole = fcfun.NutHole (nut_r = pulleynut_d/2. + 1.5*TOL,
+            h_nuthole = fcfun.NutHole (nut_r = pulleynut_d_tol/2.,
                                        nut_h = pulleynut_hole_h,
                                        hole_h = pulleydepth/2. + TOL,
                                        name = name + '_nuthole',
@@ -295,21 +319,25 @@ class IdlePulleyHolder (object):
             pulley_holder.Base = pulleyhold_aux    
             pulley_holder.Tool = nuthole    
 
+            self.fco = pulley_holder
+
         doc.recompute()
             
             
         
+"""
 
 doc = FreeCAD.newDocument()
 
 idp = IdlePulleyHolder( profile_size=30.,
                         pulleybolt_d=3.,
                         holdbolt_d = 5,
-                        above_h = 5.,
-                        mindepth = 5.,
+                        above_h = 37.,
+                        mindepth = 27.5,
                         attach_dir = '-y',
-                        endstop_side = 1,
-                        endstop_posh = 4.,
+                        endstop_side = 0,
+                        endstop_posh = 9.,
                         name = "idlepulleyhold")
+"""
 
             
